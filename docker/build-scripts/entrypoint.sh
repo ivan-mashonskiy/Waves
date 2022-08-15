4eba3197d5a09b52e07ecd99c4764c6a54068ed1
Ivan Mashonskii <ivan.mashonsky@gmail.com>
2022-08-15 12:27:50 +0300

    Update entrypoint.sh

--- docker/build-scripts/entrypoint.sh
+++ docker/build-scripts/entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 shopt -s nullglob
+NETWORKS="mainnet testnet stagenet custom"
 
 logEcho() {
   echo $1 | gosu waves tee -a /var/log/waves/waves.log
 }
 
+if [[ ! -f "$WAVES_CONFIG" ]]; then
+  logEcho "Custom '$WAVES_CONFIG' not found. Using a default one for '${WAVES_NETWORK,,}' network."
+  if [[ $NETWORKS == *"${WAVES_NETWORK,,}"* ]]; then
+    # don't use indentation for heredoc because of its restrictions
 eval "cat <<EOF
-$(<$WAVES_CONFIG)
+$(</etc/waves/waves.conf.template)
 EOF" 2> /dev/null > $WAVES_CONFIG
+  else
+    echo "Network '${WAVES_NETWORK,,}' not found. Exiting."
+    exit 1
+  fi
+else
+  echo "Found custom '$WAVES_CONFIG'. Using it."
+fi
 
 [ -n "${WAVES_WALLET_PASSWORD}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.password=${WAVES_WALLET_PASSWORD}"
 [ -n "${WAVES_WALLET_SEED}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.seed=${WAVES_WALLET_SEED}"
