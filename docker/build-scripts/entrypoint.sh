6e1ba0c5363d94133816177432f2d5fc5e41de41
Ivan Mashonskii <ivan.mashonsky@gmail.com>
2022-08-15 11:52:02 +0300

    Update entrypoint.sh

--- docker/build-scripts/entrypoint.sh
+++ docker/build-scripts/entrypoint.sh
@@ -5,6 +5,10 @@ logEcho() {
   echo $1 | gosu waves tee -a /var/log/waves/waves.log
 }
 
+eval "cat <<EOF
+$(<$WAVES_CONFIG)
+EOF" 2> /dev/null > $WAVES_CONFIG
+
 [ -n "${WAVES_WALLET_PASSWORD}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.password=${WAVES_WALLET_PASSWORD}"
 [ -n "${WAVES_WALLET_SEED}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.seed=${WAVES_WALLET_SEED}"
 JAVA_OPTS="${JAVA_OPTS} -Dwaves.directory=$WVDATA"
