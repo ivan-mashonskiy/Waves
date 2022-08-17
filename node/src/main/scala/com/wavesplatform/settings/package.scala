ad222f273f271550654cac9c2b04c498a589b985
Ivan Mashonskii <ivan.mashonsky@gmail.com>
2022-08-17 13:56:02 +0300

    Update package.scala

--- node/src/main/scala/com/wavesplatform/settings/package.scala
+++ node/src/main/scala/com/wavesplatform/settings/package.scala
@@ -58,7 +58,7 @@ package object settings {
 
   def loadConfig(maybeUserConfig: Option[Config]): Config = {
     val sysProps = ConfigFactory.defaultOverrides()
-    val external = maybeUserConfig.fold(sysProps)(sysProps.withFallback).resolve()
+    val external = maybeUserConfig.fold(sysProps)(sysProps.withFallback)
 
     val cmdDefaults =
       Try(external.getConfig("waves.defaults"))
