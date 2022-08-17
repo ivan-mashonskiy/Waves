5f5435d5054bf1f0625cbe198b6d478bb560d0bd
Ivan Mashonskii <ivan.mashonsky@gmail.com>
2022-08-17 13:52:52 +0300

    Update entrypoint.sh

--- docker/build-scripts/entrypoint.sh
+++ docker/build-scripts/entrypoint.sh
@@ -5,14 +5,13 @@ logEcho() {
   echo $1 | gosu waves tee -a /var/log/waves/waves.log
 }
 
-[ -n "${WAVES_WALLET_PASSWORD}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.password=${WAVES_WALLET_PASSWORD}"
-[ -n "${WAVES_WALLET_SEED}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.seed=${WAVES_WALLET_SEED}"
-JAVA_OPTS="${JAVA_OPTS} -Dwaves.directory=$WVDATA"
+cp $WAVES_INSTALL_PATH/lib/plugins/* $WAVES_INSTALL_PATH/lib/
+
+JAVA_OPTS="$JAVA_OPTS -Dwaves.defaults.blockchain.type=$WAVES_NETWORK"
 
 logEcho "Node is starting..."
 logEcho "WAVES_HEAP_SIZE='${WAVES_HEAP_SIZE}'"
 logEcho "WAVES_LOG_LEVEL='${WAVES_LOG_LEVEL}'"
-logEcho "WAVES_NETWORK='${WAVES_NETWORK}'"
 logEcho "JAVA_OPTS='${JAVA_OPTS}'"
 
 JAVA_OPTS="-Dlogback.stdout.level=${WAVES_LOG_LEVEL}
